a65907181e9671e97ed7ddff8e603256890c7794
matthewsolle <matthewsolle@gmail.com>
2016-06-30 15:48:40 +0100

    initial commit

---
+++ service/moving-people-safely/data.js
@@ -0,0 +1,227 @@
+var data = {
+  "service": "Moving People Safely", // service title
+  "last-updated": "June 2016", // date last updated
+  "userjourneys": [
+    {
+      "title": "Home", // user journey title
+      "path": [ // array of images
+        {
+          "caption": "Home - no moves", // image caption
+          "imgref": "images/home/01.png" // image path
+        },
+        {
+          "caption": "Search - no profile",
+          "imgref": "images/home/02.png"
+        },
+        {
+          "caption": "Invalid prisoner number",
+          "imgref": "images/home/03.png"
+        },
+        {
+          "caption": "Search - profile and move",
+          "imgref": "images/home/04.png"
+        },
+        {
+          "caption": "Search - profile, no move",
+          "imgref": "images/home/05.png"
+        },
+        {
+          "caption": "Search - profile, move, dashboard",
+          "imgref": "images/home/06.png"
+        },
+        {
+          "caption": "Personal details",
+          "imgref": "images/home/07.png"
+        },
+        {
+            "caption": "Move details",
+          "imgref": "images/home/08.png"
+        },
+        {
+            "caption": "Move details - must/must not return",
+          "imgref": "images/home/09.png"
+        },
+      ]
+    },
+    {
+      "title": "Profile", // user journey title
+      "path": [ // array of images
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/01.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/02.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/03.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/04.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/05.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/06.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/07.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/08.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/09.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/10.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/11.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/12.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/13.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/14.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/15.png" // image path
+        },
+        {
+          "caption": "Profile", // image caption
+          "imgref": "images/profile/16.png" // image path
+        },
+      ]
+    },
+    {
+      "title": "Risk", // user journey title
+      "path": [ // array of images
+        {
+          "caption": "Risk to self", // image caption
+          "imgref": "images/risk/01.png" // image path
+        },
+        {
+          "caption": "Risk from others", // image caption
+          "imgref": "images/risk/02.png" // image path
+        },
+        {
+          "caption": "Risk to others", // image caption
+          "imgref": "images/risk/03.png" // image path
+        },
+        {
+          "caption": "Risk to others", // image caption
+          "imgref": "images/risk/04.png" // image path
+        },
+        {
+          "caption": "Risk to others", // image caption
+          "imgref": "images/risk/05.png" // image path
+        },
+        {
+          "caption": "Risk to others", // image caption
+          "imgref": "images/risk/06.png" // image path
+        },
+        {
+          "caption": "Security", // image caption
+          "imgref": "images/risk/07.png" // image path
+        },
+        {
+          "caption": "Substance misuse", // image caption
+          "imgref": "images/risk/08.png" // image path
+        },
+        {
+          "caption": "Concealed weapons", // image caption
+          "imgref": "images/risk/09.png" // image path
+        },
+        {
+          "caption": "Arson/damage to property", // image caption
+          "imgref": "images/risk/10.png" // image path
+        },
+        {
+          "caption": "Communication/language", // image caption
+          "imgref": "images/risk/11.png" // image path
+        },
+      ]
+    },
+    {
+      "title": "Healthcare", // user journey title
+      "path": [ // array of images
+        {
+          "caption": "Physical healthcare", // image caption
+          "imgref": "images/healthcare/01.png" // image path
+        },
+        {
+          "caption": "Mental healthcare", // image caption
+          "imgref": "images/healthcare/02.png" // image path
+        },
+        {
+          "caption": "Social healthcare", // image caption
+          "imgref": "images/healthcare/03.png" // image path
+        },
+        {
+          "caption": "Allergies", // image caption
+          "imgref": "images/healthcare/04.png" // image path
+        },
+        {
+          "caption": "Medical health", // image caption
+          "imgref": "images/healthcare/05.png" // image path
+        },
+        {
+          "caption": "Transport", // image caption
+          "imgref": "images/healthcare/06.png" // image path
+        },
+        {
+          "caption": "Contact", // image caption
+          "imgref": "images/healthcare/07.png" // image path
+        },
+      ]
+    },
+    {
+      "title": "Offences", // user journey title
+      "path": [ // array of images
+        {
+          "caption": "Offences", // image caption
+          "imgref": "images/offences/01.png" // image path
+        },
+        {
+          "caption": "Offences", // image caption
+          "imgref": "images/offences/02.png" // image path
+        },
+        {
+          "caption": "Offences", // image caption
+          "imgref": "images/offences/03.png" // image path
+        },
+      ]
+    },
+    {
+      "title": "Dashboard", // user journey title
+      "path": [ // array of images
+        {
+          "caption": "Dashboard", // image caption
+          "imgref": "images/dashboard/01.png" // image path
+        },
+        {
+          "caption": "Dashboard", // image caption
+          "imgref": "images/dashboard/02.png" // image path
+        },
+      ]
+    },
+  ]}
